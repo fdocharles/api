1a794d1f93db2b9bb67c53975a548c0e4e4ee51b
fdocharles <fdocharles@gmail.com>
2021-11-27 08:02:19 -0500

    created users

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+var { users } = require("../data");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/authenticate", function (req, res, next) {
+  var user = users.filter(
+    (x) =>
+      x.username === req.query.username && x.password === req.query.password
+  );
+
+  console.log("users", user);
+
+  if (user && user.length > 0) {
+    res.send({
+      status: 200,
+      message: "Valid user",
+      data: {
+        id: user.id,
+      },
+    });
+  } else {
+    res.send({
+      status: 401,
+      message: "Username or Password Invalid",
+      data: {},
+    });
+  }
+});
+
+module.exports = router;
